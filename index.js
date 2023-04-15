fc464fe8881d84af32e05ee071587930d1711404
Brian <brian@brianpc>
2023-04-14 22:36:18 -0500

    first commit

--- index.js
+++ index.js
@@ -24,7 +24,38 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/:reqDate?", function(req, res) {
+  let dateStringExp = /^[0-9]{4}-([0-9]{1}|[0-9]{2})-([0-9]{1}|[0-9]{2})$/
+  let dateUnixExp = /[^0-9]/
+  /** If reqDate is a date string */
+  if(req.params.reqDate != undefined && Date.parse(req.params.reqDate) && req.params.reqDate.match(dateStringExp)) {
+    let reqDate = new Date(req.params.reqDate)
+    console.log("fecha solicitada: ", typeof(req.params.reqDate))
+    console.log("fecha convertida: ", typeof(reqDate))
+    let UTCDate = reqDate.toUTCString()
+    let UnixDate = Date.parse(reqDate)
+    res.json({ unix: UnixDate, utc: UTCDate })
+  } 
+  /** If reqDate is a unix date */
+  else if(req.params.reqDate != undefined && new Date(req.params.reqDate) && !req.params.reqDate.match(dateUnixExp)) {
+    let reqDate = new Date(parseInt(req.params.reqDate))
+    console.log("fecha solicitada: ", typeof(req.params.reqDate))
+    console.log("fecha convertida: ", typeof(reqDate))
+    console.log("fecha sol: ", req.params.reqDate)
+    console.log("fecha conv: ", reqDate)
+    let UTCDate = reqDate.toUTCString()
+    let UnixDate = Date.parse(reqDate)
+    res.json({ unix: UnixDate, utc: UTCDate })
+  } 
+  else if(req.params.reqDate == undefined){
+    let UTCDate = new Date().toUTCString()
+    let UnixDate = Date.parse(new Date())
+    res.json({ unix: UnixDate, utc: UTCDate })
+  }
+  else {
+    res.json({ error : "Invalid Date" })
+  }
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
